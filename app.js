7fc015438eed500fbcc260bcd13666e2d3fa266e
cColds <103373668+cColds@users.noreply.github.com>
2023-08-17 14:06:10 -0500

    Add placeholder controllers and routes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog");
 
 const app = express();
 const mongoDB = `mongodb+srv://admin:${process.env.MONGO_PASS}@cluster0.cwmmsd2.mongodb.net/local_library?retryWrites=true&w=majority`;
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
