56206ae785c13880fd4789d1356db6b84f9b64b2
cColds <103373668+cColds@users.noreply.github.com>
2023-08-19 18:39:19 -0500

    Add update genre

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -111,10 +111,37 @@ exports.genre_delete_post = asyncHandler(async (req, res, next) => {
 
 // Display Genre update form on GET.
 exports.genre_update_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre update GET");
+  const genre = await Genre.findById(req.params.id).exec();
+
+  res.render("genre_form", {
+    title: "Update Genre",
+    genre,
+  });
 });
 
 // Handle Genre update on POST.
-exports.genre_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre update POST");
-});
+exports.genre_update_post = [
+  // Validate and sanitize the name field.
+  body("name", "Genre name must contain at least 3 characters")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const genre = new Genre({ name: req.body.name, _id: req.params.id });
+
+    if (!errors.isEmpty()) {
+      res.render("genre_form", {
+        title: "Update Genre",
+        genre: { name: req.body.name },
+        errors: errors.array(),
+      });
+
+      return;
+    }
+    const updatedGenre = await Genre.findByIdAndUpdate(req.params.id, genre);
+
+    res.redirect(updatedGenre.url);
+  }),
+];
